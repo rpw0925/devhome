4154ba610827d9be3e04dcb83c0535932f9a6262
Kristen Schau <47155823+krschau@users.noreply.github.com>
2024-03-11 18:07:04 -0400

    Add WebView2 page

---
+++ tools/WebView2Tool/ViewModels/WebView2ToolViewModel.cs
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace DevHome.WebView2Tool;
+
+public class WebView2ToolViewModel
+{
+}
---
+++ tools/WebView2Tool/Views/WebView2ToolView.xaml.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using DevHome.Common;
+
+namespace DevHome.WebView2Tool.Views;
+
+public partial class WebView2ToolView : ToolPage
+{
+    public override string ShortName => "WebView2";
+
+    public WebView2ToolView()
+    {
+        this.InitializeComponent();
+    }
+}
