3cd2037aab3d2b20e0d4ba780876205f2ab72013
Kristen Schau <47155823+krschau@users.noreply.github.com>
2024-03-21 15:24:59 -0400

    make breadcrumbbar the default header datatemplate

--- tools/QuietBackgroundProcesses/DevHome.QuietBackgroundProcesses.UI/Views/QuietBackgroundProcessesPage.xaml.cs
+++ tools/QuietBackgroundProcesses/DevHome.QuietBackgroundProcesses.UI/Views/QuietBackgroundProcessesPage.xaml.cs
@@ -11,7 +11,10 @@ namespace DevHome.QuietBackgroundProcesses.UI.Views;
 /// </summary>
 public sealed partial class QuietBackgroundProcessesPage : ToolPage
 {
-    public override string ShortName => "Quiet Background Processes";
+    public override string DisplayName =>
+        new Microsoft.Windows.ApplicationModel.Resources.ResourceLoader(
+            "DevHome.QuietBackgroundProcesses.UI.pri",
+            "DevHome.QuietBackgroundProcesses.UI/Resources").GetString("NavigationPane/Content");
 
     public QuietBackgroundProcessesViewModel ViewModel
     {
