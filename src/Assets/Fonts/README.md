adddfa7230b6d1f53fa83181f001f439e021f20c
rpw0925 <132261667+rpw0925@users.noreply.github.com>
2024-05-14 22:19:26 -0700

    Delete src directory

--- src/Assets/Fonts/README.md
+++
@@ -1,3 +0,0 @@
-# Included fonts
-- Cascadia Mono (v2111.01)
-  - https://github.com/microsoft/cascadia-code/releases/tag/v2111.01
--- src/Contracts/Services/IActivationService.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-namespace DevHome.Contracts.Services;
-
-public interface IActivationService
-{
-    Task ActivateAsync(object activationArgs);
-}
